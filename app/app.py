f9b7ef956aae1f7c77dd3467010d720486fe9432
Chris Guarino <chris.francis.guarino@gmail.com>
2021-08-01 11:54:22 -0400

    Task: Created a congif file and a secure file used to store a secret key.

--- app/app.py
+++ app/app.py
@@ -7,13 +7,7 @@ from forms import ContactForm
 
 app = Flask(__name__, template_folder='templates')
 mysql = MySQL(cursorclass=DictCursor)
-
-app.config['MYSQL_DATABASE_HOST'] = 'db'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'biostatsData'
-app.config['SECRET_KEY'] = '79e39995d7d24c5f83f61f6c7089c2e3'
+app.config.from_pyfile('config.py')
 mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
