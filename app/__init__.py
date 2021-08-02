1b453108b619224e9815df433fa22cdbb4cb997c
Chris Guarino <chris.francis.guarino@gmail.com>
2021-08-01 20:26:02 -0400

    Task: Still running into issues with the app variable in the __init__

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 
+
 mysql = MySQL(cursorclass = DictCursor)
 app = Flask(__name__)
+
 __all__ = ['init_app']
 
 def init_app():
