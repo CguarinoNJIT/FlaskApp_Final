d3d76990eec00d4e3f79c3064ed2905db1b9bc9b
Chris Guarino <chris.francis.guarino@gmail.com>
2021-08-03 13:45:18 -0400

    Task: Edited the __init__.py file.

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,22 @@ from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 
 mysql = MySQL(cursorclass = DictCursor)
-app = Flask(__name__)
+
 __all__ = ['init_app']
 
 def init_app():
 
     #Initialize the Core Application
+    app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
     #Initialize the Plugins
     mysql.init_app(app)
 
     with app.app_context():
-        #Routing
+       #Routing
         import routes
+       #Register Blueprints
+
 
         return app
